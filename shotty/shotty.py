83830d75243db688c45e280d7edf414d80d7af65
Mauro De Lillo <mdelillo@edrans.com>
2019-08-26 17:13:30 -0300

    Added argument for rebooting EC2 instance

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -30,6 +30,7 @@ def snapshots():
 @snapshots.command('list')
 @click.option('--project', default=None, help="Only snapshot project (tag Project:<name>)")
 @click.option('--all', 'list_all', default=False, is_flag=True, help="List all snapshots for each volume, not just the most recent")
+
 def list_snapshots(project, list_all):
     "List EC2 snapshots"
 
